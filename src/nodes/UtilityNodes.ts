55fd9fb8ab9c74715dd2656e12f633fa4a342dc2
manish <bhushanwagh6@gmail.com>
2025-01-21 14:13:17 +0530

    adds : UV utility nodes

--- src/nodes/UtilityNodes.ts
+++ src/nodes/UtilityNodes.ts
@@ -1,7 +1,26 @@
-import { float, remap, remapClamp, hash, range, oscSine, oscSquare, oscTriangle, oscSawtooth, time } from "three/tsl";
+import {
+  float,
+  remap,
+  remapClamp,
+  hash,
+  range,
+  oscSine,
+  oscSquare,
+  oscTriangle,
+  oscSawtooth,
+  time,
+  matcapUV,
+  rotateUV,
+  uv,
+  vec2,
+  spherizeUV,
+  spritesheetUV,
+  positionWorldDirection,
+  equirectUV,
+} from "three/tsl";
 import { Input, Node, Output, schema } from "../nodl-core";
 import { z } from "zod";
-import { combineLatest, map } from "rxjs";
+import { combineLatest, map, of } from "rxjs";
 import { createVarNameForNode, evaluateInputs } from "./utils";
 
 export class Remap extends Node {
@@ -180,9 +199,8 @@ export class Range extends Node {
       code: `const ${varName} = range(${argsString})`,
       dependencies: ["range"],
     };
-  }
+  };
 }
-
 
 export class OscSine extends Node {
   public name: string = "OscSine";
@@ -214,7 +232,7 @@ export class OscSine extends Node {
       code: `const ${varName} = oscSine(${argsString})`,
       dependencies: ["oscSine", "time"],
     };
-  }
+  };
 }
 
 export class OscSquare extends Node {
@@ -247,7 +265,7 @@ export class OscSquare extends Node {
       code: `const ${varName} = oscSquare(${argsString})`,
       dependencies: ["oscSquare", "time"],
     };
-  }
+  };
 }
 
 export class OscTriangle extends Node {
@@ -280,7 +298,7 @@ export class OscTriangle extends Node {
       code: `const ${varName} = oscTriangle(${argsString})`,
       dependencies: ["oscTriangle", "time"],
     };
-  }
+  };
 }
 
 export class OscSawtooth extends Node {
@@ -313,7 +331,216 @@ export class OscSawtooth extends Node {
       code: `const ${varName} = oscSawtooth(${argsString})`,
       dependencies: ["oscSawtooth", "time"],
     };
-  }
+  };
 }
 
-export const UtilityNodes = { Remap, RemapClamp, Hash, Range, OscSine, OscSquare, OscTriangle, OscSawtooth };
+// matcapUV	UV coordinates for matcap texture.	vec2
+// rotateUV( uv, rotation, centerNode = vec2( 0.5 ) )	Rotates UV coordinates around a center point.	vec2
+// spherizeUV( uv, strength, centerNode = vec2( 0.5 ) )	Distorts UV coordinates with a spherical effect around a center point.	vec2
+// spritesheetUV( count, uv = uv(), frame = float( 0 ) )	Computes UV coordinates for a sprite sheet based on the number of frames, UV coordinates, and frame index.	vec2
+// equirectUV( direction = positionWorldDirection )	Computes UV coordinates for equirectangular mapping based on the direction vector.	vec2
+
+export class MatcapUv extends Node {
+  public name: string = "MatcapUV";
+  public inputs: Record<string, Input<any>> = {};
+  outputs = {
+    output: new Output({
+      name: "Output",
+      type: schema(z.any()),
+      observable: of(() => matcapUV),
+    }),
+  };
+  code = () => {
+    const varName = createVarNameForNode(this);
+    return {
+      code: `const ${varName} = matcapUV`,
+      dependencies: ["matcapUV"],
+    };
+  };
+}
+
+export class RotateUV extends Node {
+  public name: string = "RotateUV";
+  public inputs: Record<string, Input<any>> = {
+    uv: new Input({
+      name: "UV",
+      type: schema(z.any()),
+      defaultValue: uv,
+    }),
+    rotation: new Input({
+      name: "Rotation",
+      type: schema(z.any()),
+      defaultValue: () => 0,
+    }),
+    center: new Input({
+      name: "Center",
+      type: schema(z.any()),
+      defaultValue: () => vec2(0.5),
+    }),
+  };
+  outputs = {
+    output: new Output({
+      name: "Output",
+      type: schema(z.any()),
+      observable: combineLatest([
+        this.inputs.uv,
+        this.inputs.rotation,
+        this.inputs.center,
+      ]).pipe(
+        map((inputs) => () => rotateUV(inputs[0](), inputs[1](), inputs[2]()))
+      ),
+    }),
+  };
+  code = (args: string[]) => {
+    const argsString = Object.values(this.inputs)
+      .map((input, i) => {
+        const defaultValues = ["uv", "0", "vec2(0.5)"];
+        return !input.connected ? defaultValues[i] : args[i];
+      })
+      .filter((arg) => arg !== undefined && arg !== null)
+      .join(", ");
+    const varName = createVarNameForNode(this);
+    return {
+      code: `const ${varName} = rotateUV(${argsString})`,
+      dependencies: ["rotateUV", "uv"],
+    };
+  };
+}
+
+export class SpherizeUV extends Node {
+  public name: string = "SpherizeUV";
+  public inputs: Record<string, Input<any>> = {
+    uv: new Input({
+      name: "UV",
+      type: schema(z.any()),
+      defaultValue: () => positionWorldDirection,
+    }),
+    strength: new Input({
+      name: "Strength",
+      type: schema(z.any()),
+      defaultValue: () => 1,
+    }),
+    center: new Input({
+      name: "Center",
+      type: schema(z.any()),
+      defaultValue: () => vec2(0.5),
+    }),
+  };
+  outputs = {
+    output: new Output({
+      name: "Output",
+      type: schema(z.any()),
+      observable: combineLatest([
+        this.inputs.uv,
+        this.inputs.strength,
+        this.inputs.center,
+      ]).pipe(
+        map((inputs) => () => spherizeUV(inputs[0](), inputs[1](), inputs[2]()))
+      ),
+    }),
+  };
+  code = (args: string[]) => {
+    const argsString = Object.values(this.inputs)
+      .map((input, i) => {
+        const defaultValues = ["uv", "1", "vec2(0.5)"];
+        return !input.connected ? defaultValues[i] : args[i];
+      })
+      .filter((arg) => arg !== undefined && arg !== null)
+      .join(", ");
+    const varName = createVarNameForNode(this);
+    return {
+      code: `const ${varName} = spherizeUV(${argsString})`,
+      dependencies: ["spherizeUV", "uv"],
+    };
+  };
+}
+
+export class SpritesheetUV extends Node {
+  public name: string = "SpritesheetUV";
+  public inputs: Record<string, Input<any>> = {
+    count: new Input({
+      name: "Count",
+      type: schema(z.any()),
+      defaultValue: () => 1,
+    }),
+    uv: new Input({
+      name: "UV",
+      type: schema(z.any()),
+      defaultValue: uv,
+    }),
+    frame: new Input({
+      name: "Frame",
+      type: schema(z.any()),
+      defaultValue: () => 0,
+    }),
+  };
+  outputs = {
+    output: new Output({
+      name: "Output",
+      type: schema(z.any()),
+      observable: combineLatest([
+        this.inputs.count,
+        this.inputs.uv,
+        this.inputs.frame,
+      ]).pipe(
+        map(
+          (inputs) => () => spritesheetUV(inputs[0](), inputs[1](), inputs[2]())
+        )
+      ),
+    }),
+  };
+  code = (args: string[]) => {
+    const argsString = Object.values(this.inputs)
+      .map((input, i) => {
+        const defaultValues = ["1", "uv", "0"];
+        return !input.connected ? defaultValues[i] : args[i];
+      })
+      .filter((arg) => arg !== undefined && arg !== null)
+      .join(", ");
+    const varName = createVarNameForNode(this);
+    return {
+      code: `const ${varName} = spritesheetUV(${argsString})`,
+      dependencies: ["spritesheetUV", "uv"],
+    };
+  };
+}
+
+export class EquirectUV extends Node {
+  public name: string = "EquirectUV";
+  public inputs: Record<string, Input<any>> = {
+    // a: new Input({
+    //   name: "positionWorldDirection",
+    //   type: schema(z.any()),
+    //   defaultValue: () => positionWorldDirection,
+    // }),
+  };
+  outputs = {
+    output: new Output({
+      name: "Output",
+      type: schema(z.any()),
+      observable: of(() => equirectUV),
+    }),
+  };
+  code = () => {
+    const varName = createVarNameForNode(this);
+    return {
+      code: `const ${varName} = equirectUV`,
+      dependencies: ["equirectUV"],
+    };
+  }
+}
+export const UtilityNodes = {
+  Remap,
+  RemapClamp,
+  Hash,
+  Range,
+  OscSine,
+  OscSquare,
+  OscTriangle,
+  OscSawtooth,
+  MatcapUv,
+  RotateUV,
+  SpherizeUV,
+  SpritesheetUV,
+  EquirectUV,
+};
